a07a277f964ff54881ef7ccf2f8cd0a984295bae
mynameiswzq@163.com <inspiration159>
2023-01-30 17:34:51 +0800

    finally中有return

---
+++ src/main/java/com/test/exception/finallytest/ReturnInFinally.java
@@ -0,0 +1,37 @@
+package com.test.exception.finallytest;
+
+public class ReturnInFinally {
+
+    public int testModifyBasicType(){
+
+        int i=1;
+        try {
+            System.out.println("try...");
+            i+=10;
+            return i;
+        } catch (Exception e) {
+            System.out.println("catch...");
+            return i;
+        } finally {
+            i++;
+            System.out.println("finally......");
+            System.out.println(i);
+            return i;
+        }
+    }
+
+    public StringBuilder testModifyRefType(){
+
+        StringBuilder s = new StringBuilder("hahahah");
+        try {
+            return s;
+        } catch (Exception e) {
+            System.out.println("catch...");
+            return null;
+        } finally {
+            s.append("caolu");
+            System.out.println("finally......");
+            return s;
+        }
+    }
+}
