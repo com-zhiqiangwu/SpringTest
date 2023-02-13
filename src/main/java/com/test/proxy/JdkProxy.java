e732b0f40d6117510d61cf08374b09e322f111b8
mynameiswzq@163.com <inspiration159>
2023-02-13 14:57:28 +0800

    java代理

--- src/main/java/com/test/proxy/JdkProxy.java
+++ src/main/java/com/test/proxy/JdkProxy.java
@@ -1,28 +1,45 @@
 package com.test.proxy;
 
+import service.SomeService;
+import service.impl.ISomeServiceImpl;
+
 import java.lang.reflect.InvocationHandler;
 import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
 
-public class JdkProxy  implements InvocationHandler {
-    private Object targetObj;
+public class JdkProxy {
 
-    public JdkProxy(Object targetObj) {
-        this.targetObj = targetObj;
+    private Object target;
+
+    public JdkProxy(Object target) {
+        this.target = target;
     }
 
-    /**
-     *
-     * @param proxy  被代理对象
-     * @param method  目标对象中的方法对象
-     * @param args   方法对象的参数
-     * @return
-     * @throws Throwable
-     */
-    @Override
-    public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
-        System.out.println("jdk:执行方法之前的处理");
-        Object invoke = method.invoke(targetObj, args);
-        System.out.println("jdk:执行方法之后的处理");
-        return invoke;
+    public Object getProxy(){
+        return Proxy.newProxyInstance(new ISomeServiceImpl().getClass().getClassLoader(), new ISomeServiceImpl().getClass().getInterfaces(),
+                new InvocationHandler() {
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                System.out.println("日志方法执行了");
+                Object invoke = method.invoke(new ISomeServiceImpl(), args);
+                System.out.println("事务方法执行了");
+                return invoke;
+            }
+        });
+    }
+    public static void main(String[] args) {
+        /*jdk动态代理*/
+        // 代理对象不需要实现接口，但是目标对象一定要实现接口，否则不能用动态代理。
+        // 动态代理的方式中，所有的函数调用最终都会经过 invoke 函数的转发，因此我们就可以在这里做一些自己想做的操作，比如日志系统、事务、拦截器、权限控制等
+        // JDK 动态代理有一个最致命的问题是它只能代理实现了某个接口的实现类，并且代理类也只能代理接口中实现的方法，要是实现类中有自己私有的方法，而接口中没有的话，该方法不能进行代理调用。
+
+        //注意该方法(newProxyInstance)是在Proxy类中是静态方法，且接收的三个参数依次为：
+        //ClassLoader loader：指定当前目标对象使用类加载器，获取加载器的方法是固定的。
+        // Class<?>[] interfaces：目标对象实现的接口的类型，使用泛型方式确认类型。
+        // InvocationHandler h：事件处理，执行目标对象的方法时，会触发事件处理器的方法，会把当前执行目标对象的方法作为参数传入。
+
+        ISomeServiceImpl iSomeService = new ISomeServiceImpl();
+        SomeService proxy = (SomeService) new JdkProxy(iSomeService).getProxy();
+        proxy.doSome();
     }
 }
--- src/main/java/com/test/proxy/MyMethodInterceptor.java
+++ src/main/java/com/test/proxy/MyMethodInterceptor.java
@@ -1,16 +1,29 @@
 package com.test.proxy;
 
+import org.springframework.cglib.proxy.Enhancer;
 import org.springframework.cglib.proxy.MethodInterceptor;
 import org.springframework.cglib.proxy.MethodProxy;
+import service.SomeService;
+import service.impl.ISomeServiceImpl;
 
 import java.lang.reflect.Method;
 
 public class MyMethodInterceptor implements MethodInterceptor {
 
-    private Object target;
 
-    public MyMethodInterceptor(Object target) {
-        this.target = target;
+    //Cglib代理可以称为子类代理，是在内存中构建一个子类对象，从而实现对目标对象功能的扩展。
+    //C代理商不仅想代理公司，而且还想代理多个工厂的产品。
+    //Cglib通过Enhancer 来生成代理类，通过实现MethodInterceptor接口，
+    // 并实现其中的intercept方法，在此方法中可以添加增强方法，并可以利用反射Method或者MethodProxy继承类 来调用原方法。
+    public Object getCGlibProxy(Class c){
+        //工具类
+        Enhancer enhancer = new Enhancer();
+        //设置父类
+        enhancer.setSuperclass(c);
+        //设置回调函数
+        enhancer.setCallback(new MyMethodInterceptor());
+        //创建子类（代理对象）
+        return enhancer.create();
     }
 
     /**
@@ -28,5 +41,10 @@ public class MyMethodInterceptor implements MethodInterceptor {
         Object result = methodProxy.invokeSuper(o, objects);
         System.out.println("cglib:调用方法之后的处理");
         return result;
+    }
+
+    public static void main(String[] args) {
+        SomeService cGlibProxy = (SomeService) new MyMethodInterceptor().getCGlibProxy(ISomeServiceImpl.class);
+        cGlibProxy.doSome();
     }
 }
